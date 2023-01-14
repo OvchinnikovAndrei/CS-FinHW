2c72163d23e36c57554731f5b0d3591e18888d48
Andrey Ovchinnikov <sunnyhand@bk.ru>
2023-01-14 23:11:54 +0300

    Остался Аккерман

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+
+Console.WriteLine("Введите положительное число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int M = 1;
+string NaturalRec (int M, int N)
+{
+    if (M == N)
+    {
+        return N.ToString();
+    }
+    return (N+" "+NaturalRec(M, N - M));
+}
+Console.WriteLine(NaturalRec(M,N));
--- task66/Program.cs
+++ task66/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+
+
+Console.WriteLine("Введите положительное число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите положительное число M: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма элементов: {NaturalSum(M, N)}");
+
+int NaturalSum(int M, int N)
+{
+    if (M == N)
+        return N;
+    return N + NaturalSum(M, N - 1);
+}
